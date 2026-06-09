fc9824b3ebe4e4067747c41d12b6a2250f6acd55
Ahmed_AboElYazeed <aboelyazeed7777@gmail.com>
2026-06-09 23:36:49 +0300

    correct a delay

--- rpi/main_rpi.cpp
+++ rpi/main_rpi.cpp
@@ -25,7 +25,7 @@ int main() {
     std::cout << "[OTA] Service registered. Waiting for laptop...\n";
 
     while (true)
-        std::this_thread::sleep_for(std::chrono::seconds(120));
+        std::this_thread::sleep_for(std::chrono::seconds(5));
 
     return 0;
 }
